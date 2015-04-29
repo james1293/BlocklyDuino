5d87f97e6d18748f780d79bad7f44b408050c6b5
elabbiglubu <james.da.4th@gmail.com>
2015-04-28 20:56:27 -0400

    initial commit

---
+++ blockly/generators/arduino/james1293.js
@@ -0,0 +1,40 @@
+//http://www.seeedstudio.com/wiki/GROVE_System
+//http://www.seeedstudio.com/depot/index.php?main_page=advanced_search_result&search_in_description=1&keyword=grovefamily
+//support starter bundle example http://www.seeedstudio.com/wiki/GROVE_-_Starter_Kit_V1.1b
+
+/**
+ * Visual Blocks Language
+ *
+ * Copyright 2012 Fred Lin.
+ * https://github.com/gasolin/BlocklyDuino
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Helper functions for generating seeeduino grove blocks.
+ * @author gasolin@gmail.com (Fred Lin)
+ */
+
+goog.provide('Blockly.Arduino.grove');
+
+goog.require('Blockly.Arduino');
+
+
+Blockly.Arduino.grove_led = function() {
+  var dropdown_pin = this.getFieldValue('PIN');
+  var dropdown_stat = this.getFieldValue('STAT');
+  Blockly.Arduino.setups_['setup_green_led_'+dropdown_pin] = 'pinMode('+dropdown_pin+', OUTPUT);';
+  var code = 'digitalWrite('+dropdown_pin+','+dropdown_stat+');\n'
+  return code;
+};
